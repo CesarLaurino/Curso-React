5c4be008011e6a1b108ad0ea00b347364240a420
cesar laurino <cesarlaurino@hotmail.com>
2023-01-26 21:00:32 -0300

    curso react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edita <code>src/App.js</code> y guarda para recargar los cambios.
         </p>
